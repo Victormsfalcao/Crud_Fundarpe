7860609e2a78a3c2ff5934774ebc22c82003bc0b
Victormsfalcao <victorfalcao782@gmail.com>
2024-05-16 20:20:19 -0300

    Feat: Pasta Adicionada

---
+++ Residencia/app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from app.forms import cadastroUsuarioForm, loginForm, transacaoForm, projetoForm, processoForm
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def form(request):
+    data = {}
+    data['form'] = projetoForm()
+    return render(request, 'form.html', data)
+
+def create(request):
+    form = projetoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
